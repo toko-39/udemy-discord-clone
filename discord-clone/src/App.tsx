8849eb3a873d36fd35bef981250be3527d04ba42
toko-39 <tokotoko2122@gmail.com>
2026-01-11 13:15:30 +0900

    test

---
+++ discord-clone/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+import Sidebar from "./components/sidebar/sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      {/*sidebar*/}
+      <Sidebar />
+      {/*home*/}
+    </div>
+  );
+}
+
+export default App;
